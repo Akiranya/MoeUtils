a0cc4f3ccfd4c2cc5bf4865cd1a404874f0086e7
Andy Zeng <u6545267@anu.edu.au>
2019-08-04 18:46:13 +1000

    redesign mechanism of FoundOres. Now it only broadcasts what type of ore player has found, NOT the amount of ores they've found. The ability to announce amount will be added if an efficient algorithm is developed.

--- src/main/java/co/mcsky/foundores/BlockBreakListener.java
+++ src/main/java/co/mcsky/foundores/BlockBreakListener.java
@@ -3,87 +3,66 @@ package co.mcsky.foundores;
 import co.mcsky.MoeConfig;
 import co.mcsky.MoeUtils;
 import co.mcsky.utils.MoeLib;
-import com.sk89q.worldedit.bukkit.BukkitAdapter;
-import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldedit.regions.CuboidRegion;
-import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.world.World;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class BlockBreakListener implements Listener {
     final private MoeUtils moe;
-    /**
-     * K = block_type<br>
-     * V = color_code
-     */
+    // K -> block_type
+    // V -> color_code
     final private Map<Material, String> blockTypeMap;
-    /**
-     * <p>Block types which need to broadcast when players break them.</p>
-     * <p>Using HashSet for constant searching time.</p>
-     */
-    final private Set<Material> blockTypes;
-    /**
-     * <p>Stores locations of blocks (ores) where players have explored.</p>
-     * <p>The reason why I use Deque will be explained someday later :)</p>
-     */
-    final private Deque<Region> foundLocations;
-    private RegionHandler regionHandler;
+    final private Queue<PlayerBlockPair> discoveredLocation; // Stores locations of blocks (ores) where players have explored.
 
     public BlockBreakListener(MoeUtils moe) {
         this.moe = moe;
         blockTypeMap = moe.config.foundores_block_types;
-        blockTypes = blockTypeMap.keySet();
-        foundLocations = new LinkedList<>();
+        discoveredLocation = new LinkedList<>();
         if (moe.config.foundores_on) {
             moe.getServer().getPluginManager().registerEvents(this, moe);
-            regionHandler = new RegionHandler();
-            // Schedule a task which remove the last element of Deque at given interval
-            Bukkit.getScheduler().runTaskTimer(moe, () -> {
-                if (foundLocations.isEmpty()) return;
-                foundLocations.removeLast();
-            }, 0, MoeLib.toTick(moe.config.foundores_pop_interval));
+            // Schedule a task which removes the last element of Deque at given interval
+            Bukkit.getScheduler().runTaskTimer(moe, discoveredLocation::poll, 0, MoeLib.toTick(moe.config.foundores_pop_interval));
         }
     }
 
     @EventHandler
     public void onPlayerBreakBlock(BlockBreakEvent event) {
+        if (discoveredLocation.isEmpty()) {
+            return;
+        }
+
         Block block = event.getBlock();
         // Is block ore?
-        if (!blockTypes.contains(block.getType())) {
+        if (!blockTypeMap.keySet().contains(block.getType())) {
             return;
         }
-        // Is block in region previously created?
-        // If it is, simply returns, no need for further checking.
-        BlockVector3 vector = BlockVector3.at(block.getX(), block.getY(), block.getZ());
-        for (Region r : foundLocations) {
-            if (r.contains(vector)) return;
-        }
-        // Is player in survival mode?
-        if (event.getPlayer().getGameMode() != GameMode.SURVIVAL) {
+
+        Material blockType = block.getType();
+        UUID playerUUID = event.getPlayer().getUniqueId();
+        PlayerBlockPair peek = discoveredLocation.peek();
+        if (peek.player.equals(playerUUID) && peek.blockType == blockType) {
             return;
         }
+
         // Is block in enabled worlds?
         if (!moe.config.foundores_worlds.contains(block.getWorld().getName())) {
             return;
         }
+
+        // Is player in survival mode?
+//        if (event.getPlayer().getGameMode() != GameMode.SURVIVAL) {
+//            return;
+//        }
+
         // If the block is not in any region previously created,
         // adds the region which the block are from to the head of queue.
-        World world = BukkitAdapter.adapt(block.getWorld());
-        CuboidRegion region = regionHandler.createCuboidRegion(world, vector, moe.config.foundores_check_radius);
-        foundLocations.addFirst(region);
-        // And broadcast it!
-        broadcast(event, block, region);
+        discoveredLocation.add(new PlayerBlockPair(playerUUID, blockType));
+        broadcast(event, block); // Then broadcast it!
     }
 
     /**
@@ -92,15 +71,29 @@ public class BlockBreakListener implements Listener {
      * @param event The event.
      * @param block The (ore) block related to this event.
      */
-    private void broadcast(BlockBreakEvent event, Block block, Region region) {
+    private void broadcast(BlockBreakEvent event, Block block) {
         String playerName = event.getPlayer().getName();
         String blockType = block.getType().name();
         String color = blockTypeMap.get(Material.matchMaterial(blockType));
-        moe.getServer().broadcastMessage(
-                MoeConfig.color(String.format(
-                        moe.config.foundores_message_found,
-                        playerName, regionHandler.countBlock((CuboidRegion) region, block.getType()), color, blockType)
-                )
-        );
+        String msg = MoeConfig.color(String.format(moe.config.foundores_message_found, playerName, color, blockType));
+        moe.getServer().broadcastMessage(msg);
     }
+
+    private class PlayerBlockPair {
+        UUID player;
+        Material blockType;
+
+        PlayerBlockPair(UUID player, Material blockType) {
+            this.player = player;
+            this.blockType = blockType;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof PlayerBlockPair)) return false;
+            return Objects.equals(player, o);
+        }
+    }
+
 }
