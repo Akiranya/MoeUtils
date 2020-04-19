b768e5ad6c74ccbe91c3a891c7c88c1d2caf90a4
Nailm <porridgen@gmail.com>
2020-04-19 14:54:28 +1000

    Adds localization of damage causes

---
+++ src/main/java/co/mcsky/utilities/DamageCauseLocalization.java
@@ -0,0 +1,47 @@
+package co.mcsky.utilities;
+
+/**
+ * Contains chinese localization for all damage causes.
+ */
+public enum DamageCauseLocalization {
+
+    BLOCK_EXPLOSION("方块爆炸"),
+    CONTACT("触碰"),
+    CRAMMING("拥挤"),
+    CUSTOM("自定义"),
+    DRAGON_BREATH("龙息"),
+    DROWNING("淹死"),
+    DRYOUT("干渴"),
+    ENTITY_ATTACK("实体攻击"),
+    ENTITY_EXPLOSION("实体爆炸"),
+    ENTITY_SWEEP_ATTACK("实体横扫攻击"),
+    FALL("坠落"),
+    FALLING_BLOCK("坠落方块"),
+    FIRE("火烧"),
+    FIRE_TICK("火烧"),
+    FLY_INTO_WALL("撞墙"),
+    HOT_FLOOR("高温地板"),
+    LAVA("岩浆"),
+    LIGHTNING("闪电"),
+    MAGIC("魔法"),
+    MELTING("融化"),
+    POISON("中毒"),
+    PROJECTILE("弹射物"),
+    STARVATION("饥饿"),
+    SUFFOCATION("窒息"),
+    SUICIDE("自杀"),
+    THORNS("荆棘"),
+    VOID("虚空"),
+    WITHER("凋零");
+
+    private final String localization;
+
+    DamageCauseLocalization(String localization) {
+        this.localization = localization;
+    }
+
+    public String getLocalization() {
+        return this.localization;
+    }
+
+}
