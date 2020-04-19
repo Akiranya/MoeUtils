ca2a88687d3c7f18eef539a8401c3e7d3e825364
Nailm <porridgen@gmail.com>
2020-04-19 16:35:05 +1000

    Fixes logic error

--- src/main/java/co/mcsky/misc/OptimizedNetherPortal.java
+++ src/main/java/co/mcsky/misc/OptimizedNetherPortal.java
@@ -22,11 +22,13 @@ public class OptimizedNetherPortal implements Listener {
     public void onPlayerTeleport(PlayerPortalEvent e) {
         // Due to the location transformation between Overworld and Nether,
         // players might be teleported to a location outside of target world (e.g. Nether).
-        // Also, if that happens, players will be killed due to being outside of the world border,
-        // meaning that their death drops will be outside of the world border too.
 
-        // To avoid it, we see if the target location is outside of the world border and cancel the PlayerPortalEvent when necessary.
-        if (e.getTo().getWorld().getWorldBorder().isInside(e.getTo())) {
+        // Also, if that happens, players will be killed due to being outside of the world border,
+        // meaning that their death drops will be outside of the world border too, which is a pain to players.
+
+        // To avoid it, we see if the target location is outside of the target world border,
+        // and cancel the PlayerPortalEvent when it is.
+        if (!e.getTo().getWorld().getWorldBorder().isInside(e.getTo())) {
             e.setCancelled(true);
             e.getPlayer().sendMessage(moe.safePortalConfig.msg_cancelled);
             if (moe.safePortalConfig.debug) {
