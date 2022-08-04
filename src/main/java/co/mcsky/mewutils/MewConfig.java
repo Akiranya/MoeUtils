d3414e1752b61c795a73c0d21ff190c91784eeb6
Nailm <porridgen@gmail.com>
2022-08-04 09:28:58 +0800

    Remove unused config

--- src/main/java/co/mcsky/mewutils/MewConfig.java
+++ src/main/java/co/mcsky/mewutils/MewConfig.java
@@ -98,7 +98,6 @@ public final class MewConfig {
             purge_interval = foundOresNode.node("purge-interval").getInt(1800);
             enabled_blocks = foundOresNode.node("blocks").getList(Material.class, () -> List.of(Material.DIAMOND_ORE));
             enabled_worlds = foundOresNode.node("worlds").getList(String.class, () -> List.of("world"));
-            non_listener_expiry_hours = foundOresNode.node("non-listener-expiry-hours").getInt(6);
 
             final CommentedConfigurationNode magicTimeNode = root.node("magic-time");
             magic_time_cooldown = magicTimeNode.node("cooldown").getInt(600);
